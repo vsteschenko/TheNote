9581aea5e90893bb80694e050be755868484b974
Slava <steschenko.vyacheslav@gmail.com>
2024-06-06 16:22:00 +0200

    Added uploading image

--- back-end/theNote/website/views.py
+++ back-end/theNote/website/views.py
@@ -3,6 +3,13 @@ from .models import Note
 from .serializers import NoteSerializer
 from .permissions import IsOwnerOrStaffOrReadOnly
 from django.shortcuts import render
+import os 
+import cloudinary
+from cloudinary.uploader import upload
+from rest_framework.decorators import action
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.response import Response
+from rest_framework import status
 
 class NoteViewSet(ModelViewSet):
     serializer_class = NoteSerializer
@@ -19,6 +26,29 @@ class NoteViewSet(ModelViewSet):
     def perform_create(self, serializer):
         serializer.validated_data["owner"] = self.request.user
         serializer.save()
+
+    @action(detail=False, methods=['POST'], parser_classes=[MultiPartParser, FormParser])
+    def upload_image(self, request):
+        image = request.FILES.get('image')
+        text = request.data.get('text', '')
+        if not image:
+            return Response({"detail": "No image provided."}, status=status.HTTP_400_BAD_REQUEST)
+        
+        cloudinary.config( 
+            cloud_name = os.getenv("cloud_name"), 
+            api_key = os.getenv("api_key"), 
+            api_secret = os.getenv("api_secret"),
+            secure=True
+        )
+
+        upload_result = upload(image)
+        image_url = upload_result["secure_url"]
+
+        note = Note.objects.create(image_url=image_url, text=text, owner=request.user)
+        note.save()
+
+        serializer = self.get_serializer(note)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 def auth(request):
